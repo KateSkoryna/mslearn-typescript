be9acaffce434c91ad7a2a62979403da79bf4ee7
“Kateryna <k.skoryna@gmail.com>
2024-05-03 07:54:27 +0200

    add module 7 exercise 2

--- Main.ts
+++ Main.ts
@@ -6,7 +6,7 @@ if (result.error) {
     throw result.error;
 }
 
-console.log(result.parsed);
+console.log(result.parsed);  // Returns { DB_HOST: 'localhost', WEB_HOST: 'staging.adventure-works.com' }
+
 console.log(process.env.DB_HOST);
-console.log(process.env.DB_USER);
-console.log(process.env.DB_PASS);
+console.log(process.env.WEB_HOST);
